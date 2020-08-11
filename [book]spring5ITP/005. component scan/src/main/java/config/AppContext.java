a9bdca6853c2e23449ab1bec6e422f6eb20908a4
huewilliams <4879sinwoo@naver.com>
2020-08-11 23:03:09 +0900

    🍃5ITP [ADD] 5-2. @ComponentScan 애노테이션으로 스캔 설정

--- [book]spring5ITP/005. component scan/src/main/java/config/AppContext.java
+++ [book]spring5ITP/005. component scan/src/main/java/config/AppContext.java
@@ -1,58 +1,27 @@
 package config;
 
-import dao.MemberDao;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import service.ChangePasswordService;
-import service.MemberRegisterService;
 import util.*;
 
 @Configuration
+// @Component 어노테이션을 붙인 클래스를 스캔해서 스프링 빈으로 등록하려면 설정 클래스에 @ComponentScan 어노테이션을 적용해야 한다.
+// basePackages 속성 값은 스캔 대상 패키지 목록을 지정한다. 대상 패키지와 그 하위 패키지에 속한 클래스를 스캔 대상으로 설정한다.
+// 스캔 대상에 해당하는 클래스 중에서 @Component 어노테이션이 붙은 클래스의 객체를 생성해서 빈으로 등록한다.
+@ComponentScan(basePackages = {"dao", "util", "service"})
 public class AppContext {
 
     @Bean
-    public MemberDao memberDao() {
-        return new MemberDao();
-    }
-
-//    @Bean
-//    public MemberPrinter memberPrinter() {
-//        return new MemberPrinter();
-//    }
-
-    @Bean
-    // 한정자를 printer로 지정한다. 이렇게 지정한 한정자는 @Autowired에서 자동 주입할 빈을 한정할 때 사용된다.
-    // @Qualifier로 한정자를 지정하지 않으면 빈 이름이 한정자가 된다.
     @Qualifier("printer")
     public MemberPrinter memberPrinter1() {
         return new MemberPrinter();
     }
 
     @Bean
-    public MemberPrinter memberPrinter2() {
+    public MemberSummaryPrinter memberPrinter2() {
         return new MemberSummaryPrinter();
-    }
-
-    @Bean
-    public MemberRegisterService memberRegisterService() {
-        return new MemberRegisterService();
-    }
-
-    // 의존을 주입하지 않아도 스프링이 @Autowired가 붙은 필드에 해당 타입의 Bean 객체를 찾아서 주입한다.
-    @Bean
-    public ChangePasswordService changePasswordService() {
-        return new ChangePasswordService();
-    }
-
-    @Bean
-    public MemberListPrinter listPrinter() {
-        return new MemberListPrinter();
-    }
-
-    @Bean
-    public MemberInfoPrinter infoPrinter() {
-        return new MemberInfoPrinter();
     }
 
     @Bean
